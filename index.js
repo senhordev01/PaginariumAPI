da3cbc298c77ab50b2e1dd5471b24d122107378a
Rogerio <contaprogramacao230@gmail.com>
2026-09-09 16:23:49 -0300

    mudança dentro package.json

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 //node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
+//node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
 //Mudança feita para a prova de aplicativos nativos
+
 const chave = Buffer.from(process.env.CHAVE_CRYPTO, 'hex');
 
 function checar_token(req, res, next) {
