e9920347cc5bee242cbfdfec4e0581229481287f
Rogerio <contaprogramacao230@gmail.com>
2026-09-09 16:23:14 -0300

    Preparar API para deploy

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const porta = 8080;
 app.use(express.json());
 app.use(cors());
 
-
 //node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
-
+//Mudança feita para a prova de aplicativos nativos
 const chave = Buffer.from(process.env.CHAVE_CRYPTO, 'hex');
 
 function checar_token(req, res, next) {
